d5b31f2f5694c1d9e1547d8b1109cffc3d9c40ef
TaniahAndria <t.andriambololona@yahoo.fr>
2025-11-03 21:00:59 +0300

    Matching stage final

--- src/main/java/itu/mbds/collaborativecommutingapi/services/match/MatchingServiceImpl.java
+++ src/main/java/itu/mbds/collaborativecommutingapi/services/match/MatchingServiceImpl.java
@@ -42,7 +42,7 @@ public class MatchingServiceImpl implements IMatchingService {
         return exactMatch || proximityMatch;
     }
     private double distance(double lat1, double lon1, double lat2, double lon2) {
-        final int R = 6371; // Rayon de la Terre en km
+        final int R = 6371;
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
